2ce499365ffb1a45cf922cc99a568036f27b2ed2
Nino Guinot Toffoli <ninoguinottoffoli@mac-1.home>
2025-12-19 20:08:55 +0100

    blabla

---
+++ ex00/main.cpp
@@ -0,0 +1,15 @@
+#include "Zombie.hpp"
+
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
+
+int main()
+{
+    Zombie* heapZombie = newZombie("Foo");
+    heapZombie->announce();
+    delete heapZombie;
+
+    randomChump("Bar");
+
+    return (0);
+}
